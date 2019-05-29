477d3ae70dc3556412baf9137310459ef17de75a
Dmitriy Kalinin <cppforlife@gmail.com>
2019-05-29 21:32:18 +0100

    change hack/test.sh to sync files via git

--- hack/test.sh
+++ hack/test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-set -e
+set -e -x
 
 rm -rf ./tmp
 mkdir ./tmp
 
+git commit --allow-empty -am './hack/test.sh'
 git clone . ./tmp/_ytt_lib/github.com/k14s/k8s-lib
+git reset --soft head~1
 cp -r examples/ ./tmp
 
 ytt -f ./tmp --file-mark app.yml:exclusive-for-output=true
 ytt -f ./tmp --file-mark app-with-volumes.yml:exclusive-for-output=true
 ytt -f ./tmp --file-mark app-with-overlay.yml:exclusive-for-output=true
+ytt -f ./tmp --file-mark nsa.yml:exclusive-for-output=true
+
+echo "SUCCESS (but also STAGED ALL FILES IN GIT)"
